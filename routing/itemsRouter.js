ee7b23d5d1aa0538e3f57602c72fa1451d3e8d16
Philip Dimaano <pdimaano7@gmail.com>
2022-11-02 17:39:59 -0700

    Finished routes for patch and delete diff --git a/routing/itemsRouter.js b/routing/itemsRouter.js index 32d6815..40de5b3 100644 --- a/routing/itemsRouter.js +++ b/routing/itemsRouter.js @@ -9,35 +9,60 @@ const router = new express.Router();  //End of Boilerplate
     /** GET /items: returns list of shopping items */
    -router.get("/", function(req, res){
    -    return res.json(db.items);
    -})
    +router.get("/", function (req, res) {
    +  return res.json(db.items);
    +});
    
     /** POST /items: accept JSON body, add item, return the new item */
    -router.post("/", function(req, res){
    -    db.items.push(req.body);
    -    return res.json({added : req.body});
    -})
    +router.post("/", function (req, res) {
    +  db.items.push(req.body);
    +  return res.json({ added: req.body });
    +});
    
     /** GET /items/:name: returns a single item from the database.*/
    -router.get("/:name", function(req, res){
    -    let result = findItem(db.items, req.params.name)
    -    // debugger;
    -    return res.json(result);
    -})
    +router.get("/:name", function (req, res) {
    +  let result = findItem(db.items, req.params.name);
    +  // debugger;
    +  return res.json(result);
    +});
    
     /** Function which accepts array of items in database. Iterates through array
      *  of objects to find the matching keyword. Returns the object of interest.
    -*/
    + */
     function findItem(items, keyword) {
    -    for (let obj of items) {
    -        if (obj["name"] === keyword) {
    -            return obj;
    -        }
    +  for (let obj of items) {
    +    if (obj["name"] === keyword) {
    +      return obj;
         }
    +  }
    +}
    +
    +/** PATCH /items: accept JSON body, modify item and return it */
    +router.patch("/:name", function (req, res) {
    +  let result = findItem(db.items, req.params.name);
    +  result.name = req.body.name;
    +  db.items.push(req.body);
    +  return res.json({ updated: req.body });
    +});
    +
    +
    +/** DELETE /:name delete item, return {message: Deleted} */
    +router.delete("/:name", function (req, res) {
    +    deleteItem(req.params.name);
    +    return res.json({ message: "Deleted" });
    +  });
    +
    +/** Function which finds index of desired item in array of objects and
    + *  removes it from the array using splice. Returns remaining objects in array.
    +*/
    +
    +function deleteItem(item) {
    +    let itemIdx = db.items.indexOf(item)
    +    db.items.splice(itemIdx, 1)
    +    return db.items
     }
    
     //End Boilerplate
     module.exports = router;
    -// End of End Boilerplate
    \ No newline at end of file
    +// End of End Boilerplate

--- routing/itemsRouter.js
+++ routing/itemsRouter.js
@@ -9,32 +9,57 @@ const router = new express.Router();
 //End of Boilerplate
 
 /** GET /items: returns list of shopping items */
-router.get("/", function(req, res){
-    return res.json(db.items);
-})
+router.get("/", function (req, res) {
+  return res.json(db.items);
+});
 
 /** POST /items: accept JSON body, add item, return the new item */
-router.post("/", function(req, res){
-    db.items.push(req.body);
-    return res.json({added : req.body});
-})
+router.post("/", function (req, res) {
+  db.items.push(req.body);
+  return res.json({ added: req.body });
+});
 
 /** GET /items/:name: returns a single item from the database.*/
-router.get("/:name", function(req, res){
-    let result = findItem(db.items, req.params.name)
-    // debugger;
-    return res.json(result);
-})
+router.get("/:name", function (req, res) {
+  let result = findItem(db.items, req.params.name);
+  // debugger;
+  return res.json(result);
+});
 
 /** Function which accepts array of items in database. Iterates through array
  *  of objects to find the matching keyword. Returns the object of interest.
-*/
+ */
 function findItem(items, keyword) {
-    for (let obj of items) {
-        if (obj["name"] === keyword) {
-            return obj;
-        }
+  for (let obj of items) {
+    if (obj["name"] === keyword) {
+      return obj;
     }
+  }
+}
+
+/** PATCH /items: accept JSON body, modify item and return it */
+router.patch("/:name", function (req, res) {
+  let result = findItem(db.items, req.params.name);
+  result.name = req.body.name;
+  db.items.push(req.body);
+  return res.json({ updated: req.body });
+});
+
+
+/** DELETE /:name delete item, return {message: Deleted} */
+router.delete("/:name", function (req, res) {
+    deleteItem(req.params.name);
+    return res.json({ message: "Deleted" });
+  });
+
+/** Function which finds index of desired item in array of objects and
+ *  removes it from the array using splice. Returns remaining objects in array.
+*/
+
+function deleteItem(item) {
+    let itemIdx = db.items.indexOf(item)
+    db.items.splice(itemIdx, 1)
+    return db.items
 }
 
 

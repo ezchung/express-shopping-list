b21cd4738e492a5b0b3aee75e03cf645469d555c
Philip Dimaano <pdimaano7@gmail.com>
2022-11-02 17:03:37 -0700

    Finished route for getting single item from database diff --git a/routing/itemsRouter.js b/routing/itemsRouter.js index 5d60eee..32d6815 100644 --- a/routing/itemsRouter.js +++ b/routing/itemsRouter.js @@ -19,6 +19,25 @@ router.post("/", function(req, res){      return res.json({added : req.body});  })
    +/** GET /items/:name: returns a single item from the database.*/
    +router.get("/:name", function(req, res){
    +    let result = findItem(db.items, req.params.name)
    +    // debugger;
    +    return res.json(result);
    +})
    +
    +/** Function which accepts array of items in database. Iterates through array
    + *  of objects to find the matching keyword. Returns the object of interest.
    +*/
    +function findItem(items, keyword) {
    +    for (let obj of items) {
    +        if (obj["name"] === keyword) {
    +            return obj;
    +        }
    +    }
    +}
    +
    +
     //End Boilerplate
     module.exports = router;
     // End of End Boilerplate
    \ No newline at end of file

--- routing/itemsRouter.js
+++ routing/itemsRouter.js
@@ -19,6 +19,25 @@ router.post("/", function(req, res){
     return res.json({added : req.body});
 })
 
+/** GET /items/:name: returns a single item from the database.*/
+router.get("/:name", function(req, res){
+    let result = findItem(db.items, req.params.name)
+    // debugger;
+    return res.json(result);
+})
+
+/** Function which accepts array of items in database. Iterates through array
+ *  of objects to find the matching keyword. Returns the object of interest.
+*/
+function findItem(items, keyword) {
+    for (let obj of items) {
+        if (obj["name"] === keyword) {
+            return obj;
+        }
+    }
+}
+
+
 //End Boilerplate
 module.exports = router;
 // End of End Boilerplate
